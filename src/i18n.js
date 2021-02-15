5e21cb7f64dbb7ba0bdd0abb5ae66293df9ac31b
bnulens <brechtnulens@gmail.com>
2021-02-15 19:52:36 +0100

    First draft translations

--- src/i18n.js
+++ src/i18n.js
@@ -11,7 +11,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "nl",
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
