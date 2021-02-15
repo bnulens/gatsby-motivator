a06b0c39fe8921a240a5d0d0f74d42dff62f9459
bnulens <brechtnulens@gmail.com>
2021-02-15 16:30:23 +0100

    Reworked background & tilted canvas in layout

--- src/style/layout.js
+++ src/style/layout.js
@@ -1,78 +1,49 @@
-import React, { useState } from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react"
 import styled from "styled-components"
 import PropTypes from "prop-types"
 
-import Flex from "../components/Flex"
-import Header from "../components/header"
-import Footer from "../components/footer"
-import ToggleNightMode from "../components/ToggleNightMode"
-
-const Container = styled(Flex)`
-  height: 100%;
-  align-items: stretch;
-  justify-content: center;
-  @media ${(props) => props.theme.mediaQueries.large} {
-    align-items: center;
-    height: 100vh;
-    ::before {
-      content: "";
-      position: absolute;
-      background: ${(props) => (props.darkMode ? "#555" : "grey")};
-      width: 100%;
-      height: 100%;
-      z-index: -6;
-    }
-  }
-`
-const StyledWrapper = styled.div`
+const BackDrop = styled.div`
+  display: block;
   position: relative;
-  max-width: 900px;
-  height: auto;
-  padding: 0 16px;
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(
+    160deg,
+    var(--color-purple_prime),
+    var(--color-red_prime)
+  );
+  overflow: hidden;
+`
 
-  @media ${(props) => props.theme.mediaQueries.large} {
-    position: relative;
-    width: 868px;
-    margin: -72px auto 0px;
+const Container = styled.div`
+  margin: 3.5% auto;
+  max-width: 90%;
+  color: var(--color-white);
 
-    ::before {
-      content: "";
-      position: absolute;
-      background: #41b883;
-      width: 100%;
-      height: 120%;
-      top: -10px;
-      left: 0;
-      transform: rotate(3deg);
-      -webkit-transform: rotate(3deg);
-      -moz-transform: rotate(3deg);
-      z-index: -4;
-    }
+  ::before {
+    content: "";
+    position: absolute;
+    top: 3.5vw;
+    left: -12, 5%;
+    height: 86vh;
+    width: 100%;
+    max-width: 90%;
+    background-image: linear-gradient(
+      270deg,
+      var(--color-red_second),
+      var(--color-red_prime)
+    );
+    transform: rotate(2deg);
   }
 `
 
 export default function Layout({ children }) {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-  const [darkMode, setDarkMode] = useState(false)
   return (
-    <Container>
-      <StyledWrapper>
-        <Header siteTitle={data.site.siteMetadata.title}>
-          <ToggleNightMode value={darkMode} setDark={setDarkMode} />
-        </Header>
+    <BackDrop>
+      <Container>
         <main>{children}</main>
-        <Footer />
-      </StyledWrapper>
-    </Container>
+      </Container>
+    </BackDrop>
   )
 }
 
