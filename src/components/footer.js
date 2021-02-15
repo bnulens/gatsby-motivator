e9c3ad5dd1bc97e97f351dd01f69566f6c4e8bc2
bnulens <brechtnulens@gmail.com>
2021-02-15 16:25:00 +0100

    Header & footer rework

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,31 +1,11 @@
 import React from "react"
-import styled from "styled-components"
-
-import Flex from "./Flex"
-
-const StyledFooter = styled.footer`
-  position: relative;
-`
-const FooterWrapper = styled(Flex)`
-  position: absolute;
-  background: #adffce;
-  height: 50px;
-  width: 100%;
-  margin-left: -16px;
-  span {
-    margin: 0 auto;
-    color: grey;
-  }
-`
 
 const Footer = () => {
   const thisYear = new Date().getFullYear()
   return (
-    <StyledFooter>
-      <FooterWrapper alignItems="center">
-        <span>&copy; {thisYear}</span>
-      </FooterWrapper>
-    </StyledFooter>
+    <>
+      <p>{thisYear}</p>
+    </>
   )
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,73 +1,28 @@
 import React from "react"
-import PropTypes from "prop-types"
-import styled from "styled-components"
+import { useStaticQuery, graphql } from "gatsby"
 
-import Flex from "./Flex"
-import HeaderNav from "./navbar/Navbar"
-import ProfilePicture from "./ProfilePicture"
+import Navbar from "../components/navbar/Navbar"
 
-const StyledHeader = styled.header`
-  margin-bottom: 32px;
-  ::before {
-    content: "";
-    position: absolute;
-    background: #adffce;
-    width: 100%;
-    height: 152px;
-    left: 0;
-    clip-path: polygon(0 0, 100% 0%, 100% 50%, 0 100%);
-    -webkit-clip-path: polygon(0 0, 100% 0, 100% 50%, 0 100%);
-  }
-  @media screen and (min-width: 900px) {
-    width: 80%;
-    height: 180px;
-    margin: 0 auto;
-  }
-`
+import { StyledHeader } from "../style/elements/styledHeader"
 
-const TitleWrapper = styled(Flex)`
-  position: relative;
-`
-
-const MainTitle = styled.h1`
-  width: 50%;
-  font-size: 40px;
-  padding-top: 24px;
-  .title-first-name {
-    font-weight: 500;
-    text-transform: uppercase;
-  }
-  .title-last-name {
-    font-weight: 100;
-    text-transform: uppercase;
-    letter-spacing: 2px;
-  }
-`
-const StyledHeaderNav = styled(HeaderNav)`
-  position: relative;
-`
-
-const Header = ({ children }) => (
-  <>
-    <StyledHeader>
-      <TitleWrapper justifyContent="space-between" alignItems="center">
-        <MainTitle>
-          <span className="title-first-name">Brecht </span>
-          <span className="title-last-name">Nulens</span>
-        </MainTitle>
-        <ProfilePicture />
-      </TitleWrapper>
-      <StyledHeaderNav>{children}</StyledHeaderNav>
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  return (
+    <StyledHeader siteTitle={data.site.siteMetadata.title}>
+      <div className="header__heading-wrapper">
+        <h2>I am Brecht Nulens</h2>
+      </div>
+      <Navbar />
     </StyledHeader>
-  </>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+  )
 }
 
 export default Header
