42507ed39d1e51d887821c6fc47031bab04b7379
bnulens <brechtnulens@gmail.com>
2021-02-16 12:15:41 +0100

    Enabled translation & added translation strings

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { useTranslation } from "react-i18next"
 
 import Navbar from "../components/navbar/Navbar"
 
 import { StyledHeader } from "../style/elements/styledHeader"
 
 const Header = () => {
+  const { t } = useTranslation()
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -18,7 +20,7 @@ const Header = () => {
   return (
     <StyledHeader siteTitle={data.site.siteMetadata.title}>
       <div className="header__heading-wrapper">
-        <h2>I am Brecht Nulens</h2>
+        <h2>{t("header.title")}</h2>
       </div>
       <Navbar />
     </StyledHeader>
