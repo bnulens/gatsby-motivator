0ca1de566e5b0acb0a2936959cd39c1ae8f63a10
bnulens <brechtnulens@gmail.com>
2021-02-17 01:35:33 +0100

    Separated texts & styled paragraphs

--- src/style/elements/styledLetter.js
+++ src/style/elements/styledLetter.js
@@ -5,25 +5,90 @@ export const StyledLetter = styled.section`
   display: flex;
   width: 100%;
   height: 86vh;
+  padding: 24px 12px;
   border-radius: 8px;
   background-color: var(--color-purple_prime);
-  overflow: scroll;
   z-index: 500;
 `
 
 export const Wrapper = styled.div`
-  width: 75%;
+  width: 80%;
   margin: 0 auto;
   padding: 12px 24px;
+  overflow: scroll;
 
   .motivation__wrapper {
     blockquote {
       display: block;
+      margin-top: 24px;
       padding: 12px 16px;
-      font-size: 18px;
+      font-size: 24px;
       font-weight: 200;
       border-left: 3px solid var(--color-red_prime);
       background-color: var(--color-purple_second);
+    }
+    blockquote:nth-of-type(2) {
+      background-color: var(--color-purple_prime);
+    }
+    .motivation__about {
+      display: flex;
+      margin: 50px 0px;
+
+      .motivation__about-heading {
+        display: flex;
+        flex-direction: column;
+        width: 35%;
+        h2 {
+          font-family: "Fraunces";
+          font-size: 42px;
+          font-weight: 300;
+          text-align: justify;
+        }
+        h3 {
+          font-size: 14px;
+          font-weight: 100;
+        }
+      }
+      .motivation__about-content {
+        display: flex;
+        flex-direction: column;
+        width: 65%;
+        p {
+          text-align: justify;
+          padding: 12px;
+          font-weight: 300;
+          margin-bottom: 12px;
+        }
+      }
+      @media ${(props) => props.theme.mediaQueries.large} {
+      }
+    }
+    .motivation__motivate {
+      display: flex;
+      .motivation__motivate-heading {
+        display: flex;
+        flex-direction: column;
+        width: 35%;
+        padding: 12px;
+        h2 {
+          font-family: "Fraunces";
+          font-size: 42px;
+          font-weight: 300;
+          text-align: justify;
+        }
+      }
+      .motivation__motivate-content {
+        display: flex;
+        flex-direction: column;
+        width: 65%;
+        padding: 12px;
+        text-align: justify;
+        p {
+          text-align: justify;
+          font-weight: 300;
+          margin-bottom: 12px;
+        }
+      }
     }
   }
 `
