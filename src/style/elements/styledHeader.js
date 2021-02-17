3b8307ccd228f3d9ea7b3b90446d6f4ad9c0181c
bnulens <brechtnulens@gmail.com>
2021-02-17 01:33:50 +0100

    Made titles of Header more visible

--- src/style/elements/styledHeader.js
+++ src/style/elements/styledHeader.js
@@ -4,11 +4,18 @@ export const StyledHeader = styled.header`
   display: sticky;
   top: 0;
   width: 100%;
+  height: auto;
 
   .header__heading-wrapper {
-    h2 {
+    margin-bottom: 16px;
+    line-height: 1.2;
+    h1 {
       font-family: "Fraunces";
-      font-size: 65px;
+      font-size: 64px;
+      font-weight: 300;
+    }
+    h2 {
+      font-size: 32px;
       font-weight: 100;
     }
   }
