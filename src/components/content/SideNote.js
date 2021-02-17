99ac040ce4bc591d6a923372452566cac9a39987
bnulens <brechtnulens@gmail.com>
2021-02-17 01:31:10 +0100

    Added translation & bulletpoints to sideNote

--- src/components/content/SideNote.js
+++ src/components/content/SideNote.js
@@ -1,17 +1,28 @@
 import React from "react"
+import { useTranslation } from "react-i18next"
 
 import LanguageSwitcher from "../LanguageSwitcher"
 import { StyledSideNote } from "../../style/elements/styledSideNote"
+
 const SideNote = () => {
+  const { t } = useTranslation()
   return (
     <StyledSideNote>
-      <LanguageSwitcher />
+      <span></span>
       <ul className="side-note__side-scroll">
-        <li className="side-note__side-scroll-item">A</li>
-        <li className="side-note__side-scroll-item">B</li>
-        <li className="side-note__side-scroll-item">C</li>
-        <li className="side-note__side-scroll-item">D</li>
+        <li className="side-note__side-scroll-item">
+          <span className="item-bullet"></span>
+          <h4>{t("body.about.title")}</h4>
+        </li>
+        <li className="side-note__side-scroll-item">
+          <span className="item-bullet"></span>
+          <h4>{t("body.motivate.title")}</h4>
+        </li>
       </ul>
+      <div className="side-note__languages">
+        <p>{t("body.sideNote")}</p>
+        <LanguageSwitcher />
+      </div>
     </StyledSideNote>
   )
 }
--- src/style/elements/styledSideNote.js
+++ src/style/elements/styledSideNote.js
@@ -3,12 +3,40 @@ import styled from "styled-components"
 export const StyledSideNote = styled.aside`
   display: flex;
   flex-direction: column;
-  width: 25%;
+  justify-content: space-between;
+  width: 20%;
+  height: 100%;
   padding: 12px 24px;
 
   .side-note__side-scroll {
     .side-note__side-scroll-item {
+      display: flex;
+      align-items: center;
+      margin: 12px auto;
+      cursor: pointer;
+
+      .item-bullet {
+        display: block;
+        width: 10px;
+        height: 10px;
+        margin-right: 10px;
+        border: 1px solid var(--color-silver);
+        border-radius: 50%;
+      }
+
+      h4 {
+        font-family: "Fraunces";
+        font-size: 24px;
+        font-weight: 300;
+        color: var(--color-silver);
+      }
+    }
+  }
+  .side-note__languages {
+    p {
       font-family: "Fraunces";
+      font-size: 12px;
+      font-weight: 300;
     }
   }
 `
