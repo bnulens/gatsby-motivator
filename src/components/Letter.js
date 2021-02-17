cb6ee08c5ff2fe09d0f1d8d30eb7e104c6a1c581
bnulens <brechtnulens@gmail.com>
2021-02-17 01:28:59 +0100

    Separated texts & styled paragraphs

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -17,17 +17,25 @@ const Letter = () => {
         <section className="motivation__wrapper">
           <blockquote>{t("body.intro")}</blockquote>
           <article className="motivation__about">
-            <h2>{t("body.about.title")}</h2>
-            <h3>{t("body.about.subTitle")}</h3>
-            <p>{t("body.about.p1")}</p>
-            <p>{t("body.about.p2")}</p>
+            <div className="motivation__about-heading">
+              <h2>{t("body.about.title")}</h2>
+              <h3>{t("body.about.subTitle")}</h3>
+            </div>
+            <div className="motivation__about-content">
+              <p>{t("body.about.p1")}</p>
+              <p>{t("body.about.p2")}</p>
+            </div>
           </article>
           <article className="motivation__motivate">
-            <h2>{t("body.motivate.title")}</h2>
-            <p>{t("body.motivate.p1")}</p>
-            <p>{t("body.motivate.p2")}</p>
-            <p>{t("body.motivate.p3")}</p>
-            <p>{t("body.motivate.p4")}</p>
+            <div className="motivation__motivate-heading">
+              <h2>{t("body.motivate.title")}</h2>
+            </div>
+            <div className="motivation__motivate-content">
+              <p>{t("body.motivate.p1")}</p>
+              <p>{t("body.motivate.p2")}</p>
+              <p>{t("body.motivate.p3")}</p>
+              <p>{t("body.motivate.p4")}</p>
+            </div>
           </article>
           <blockquote>{t("body.motivate.outro")}</blockquote>
         </section>
