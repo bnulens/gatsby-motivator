eb78ac59dcd856f8c21fc6572263fbee3d457116
bnulens <brechtnulens@gmail.com>
2021-02-20 01:54:32 +0100

    Mobile fix layout

--- src/style/elements/styledLetter.js
+++ src/style/elements/styledLetter.js
@@ -3,16 +3,21 @@ import styled from "styled-components"
 export const StyledLetter = styled.section`
   position: relative;
   display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 86vh;
   padding: 24px 12px;
-  border-radius: 8px;
   background-color: var(--color-purple_prime);
-  z-index: 500;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    height: 86vh;
+    border-radius: 8px;
+    z-index: 500;
+  }
 `
 
 export const Wrapper = styled.div`
-  width: 80%;
+  width: 100%;
   margin: 0 auto;
   padding: 12px 24px;
   overflow: scroll;
@@ -31,21 +36,25 @@ export const Wrapper = styled.div`
       background-color: var(--color-purple_prime);
       text-align: justify;
     }
+
     /* About article */
     .motivation__about {
       display: flex;
+      flex-direction: column;
       margin: 50px 0px;
 
       .motivation__about-heading {
         display: flex;
         flex-direction: column;
-        width: 35%;
+        margin-bottom: 24px;
+
         h2 {
           font-family: "Fraunces";
           font-size: 42px;
           font-weight: 300;
           text-align: justify;
         }
+
         h3 {
           font-size: 14px;
           font-weight: 100;
@@ -54,25 +63,50 @@ export const Wrapper = styled.div`
       .motivation__about-content {
         display: flex;
         flex-direction: column;
-        width: 65%;
+
         p {
           text-align: justify;
-          padding: 12px;
           font-weight: 300;
           margin-bottom: 12px;
         }
       }
-      @media ${(props) => props.theme.mediaQueries.large} {
+      @media screen and (min-width: 768px) {
+        flex-direction: row;
+
+        .motivation__about-heading {
+          width: 35%;
+          h2 {
+            font-family: "Fraunces";
+            font-size: 42px;
+            font-weight: 300;
+            text-align: justify;
+          }
+          h3 {
+            font-size: 14px;
+            font-weight: 100;
+          }
+        }
+        .motivation__about-content {
+          width: 65%;
+          p {
+            text-align: justify;
+            padding: 12px;
+            font-weight: 300;
+            margin-bottom: 12px;
+          }
+        }
       }
     }
     /* Motivation article */
     .motivation__motivate {
       display: flex;
+      flex-direction: column;
+
       .motivation__motivate-heading {
         display: flex;
         flex-direction: column;
-        width: 35%;
-        padding: 12px;
+        margin-bottom: 24px;
+
         h2 {
           font-family: "Fraunces";
           font-size: 42px;
@@ -83,34 +117,77 @@ export const Wrapper = styled.div`
       .motivation__motivate-content {
         display: flex;
         flex-direction: column;
-        width: 65%;
-        padding: 12px;
         text-align: justify;
+
         p {
           text-align: justify;
           font-weight: 300;
           margin-bottom: 12px;
         }
       }
+      @media screen and (min-width: 768px) {
+        flex-direction: row;
+
+        .motivation__motivate-heading {
+          width: 35%;
+          padding: 12px;
+        }
+        .motivation__motivate-content {
+          width: 65%;
+          padding: 12px;
+        }
+      }
     }
     /* Stack article */
     .motivation__stack-info {
+      margin: 24px auto;
+
       .motivation__stack {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr;
+        gap: 24px 6px;
+        grid-template-areas:
+          ". . . ."
+          ". . . ."
+          ". . . .";
+
         li {
           display: flex;
-          align-items: center;
-          span {
-            margin-right: 6px;
-          }
-          p {
-            font-size: 14px;
-            font-weight: 300;
+          flex-direction: row;
+          width: auto;
+        }
+
+        span {
+          margin-right: 6px;
+        }
+
+        @media screen and (min-width: 768px) {
+          display: flex;
+          justify-content: space-between;
+          width: 90%;
+          margin: 0 auto;
+
+          li {
+            display: flex;
+            align-items: center;
+
+            span {
+              margin-right: 10px;
+            }
+            p {
+              font-size: 14px;
+              font-weight: 300;
+            }
           }
         }
       }
     }
+  }
+  @media screen and (min-width: 768px) {
+    width: 90%;
+  }
+  @media screen and (min-width: 1080px) {
+    max-width: 1080px;
   }
 `
--- src/style/layout.js
+++ src/style/layout.js
@@ -4,36 +4,52 @@ import PropTypes from "prop-types"
 
 const BackDrop = styled.div`
   display: block;
-  position: relative;
-  width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(
-    160deg,
-    var(--color-purple_prime),
-    var(--color-red_prime)
-  );
-  overflow: hidden;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(
+      160deg,
+      var(--color-purple_prime),
+      var(--color-red_prime)
+    );
+    overflow: hidden;
+  }
 `
 
 const Container = styled.div`
-  margin: 7vh auto;
-  max-width: 90%;
+  height: 100vh;
   color: var(--color-white);
 
-  ::before {
-    content: "";
-    position: absolute;
-    top: 7vh;
-    left: -12, 5%;
-    height: 86vh;
-    width: 100%;
+  @media screen and (min-width: 768px) {
+    margin: 7vh auto;
     max-width: 90%;
-    background-image: linear-gradient(
-      270deg,
-      var(--color-red_second),
-      var(--color-red_prime)
-    );
-    transform: rotate(2deg);
+
+    ::before {
+      content: "";
+      position: absolute;
+      top: 7vh;
+      left: -12, 5%;
+      height: 86vh;
+      width: 100%;
+      max-width: 90%;
+      background-image: linear-gradient(
+        270deg,
+        var(--color-red_second),
+        var(--color-red_prime)
+      );
+      transform: rotate(2deg);
+    }
+  }
+  @media screen and (min-width: 1080px) {
+    max-width: 1080px;
+
+    ::before {
+      max-width: 1080px;
+    }
   }
 `
 
