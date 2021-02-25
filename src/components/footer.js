900660a03e715cb5572d4d045090758b5dd45db4
bnulens <brechtnulens@gmail.com>
2021-02-25 18:32:16 +0100

    Initial structure - added contact links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,34 @@
 import React from "react"
+import {
+  StyledFooting,
+  PhoneIcon,
+  EmailIcon,
+} from "../style/elements/styledFooter"
 
 const Footer = () => {
   const thisYear = new Date().getFullYear()
+
   return (
-    <>
-      <p>{thisYear}</p>
-    </>
+    <StyledFooting>
+      <div className="footer__inner-wrapper">
+        <h3>Let's get in touch !</h3>
+        <a href="tel:+32494749731" className="footer__contact-link">
+          <PhoneIcon />
+          <p>... by phone</p>
+        </a>
+        <a
+          href="mailto:brechtnulens@gmail.com"
+          className="footer__contact-link"
+        >
+          <EmailIcon />
+          <p>... or by email</p>
+        </a>
+        <p>
+          {`\uD83D\uDD25`}
+          {thisYear}
+        </p>
+      </div>
+    </StyledFooting>
   )
 }
 
