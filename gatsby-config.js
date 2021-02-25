67df57d4983ec727b00f08e6ad3c644146f6ea51
bnulens <brechtnulens@gmail.com>
2021-02-25 18:40:33 +0100

    Favicon path adjusted

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/profile-picture.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/profile-picture.png`, // This path is relative to the root of the site.
       },
     },
     {
