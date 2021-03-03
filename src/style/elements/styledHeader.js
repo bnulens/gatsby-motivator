24277e297c941410c135285264730904843737e5
bnulens <brechtnulens@gmail.com>
2021-03-03 14:32:58 +0100

    Merged SideNote into header comp

--- src/style/elements/styledHeader.js
+++ src/style/elements/styledHeader.js
@@ -1,22 +1,97 @@
 import styled from "styled-components"
 
+import IntIcon from "../../assets/icons/language.svg"
+
+export const LanguagesIcon = styled(IntIcon)`
+  display: block;
+  width: 24px;
+  path {
+    fill: white;
+  }
+`
+
 export const StyledHeader = styled.header`
-  display: sticky;
-  top: 0;
-  width: 100%;
-  height: auto;
+  max-width: 90%;
+  margin: 0 auto;
+  background-color: var(--color-purple_prime);
 
   .header__heading-wrapper {
-    margin-bottom: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     line-height: 1.2;
-    h1 {
-      font-family: "Fraunces";
-      font-size: 64px;
-      font-weight: 300;
+
+    .header__title-wrapper {
+      margin-bottom: 24px;
+
+      h1 {
+        font-family: "Fraunces";
+        font-size: 40px;
+        font-weight: 300;
+      }
+      h2 {
+        font-size: 16px;
+        font-weight: 100;
+      }
     }
-    h2 {
-      font-size: 32px;
-      font-weight: 100;
+  }
+  .header__languages {
+    .header__languages-note {
+      display: none;
+
+      ${LanguagesIcon} {
+        margin-left: 12px;
+      }
+
+      p {
+        font-family: "Fraunces";
+        font-size: 12px;
+        font-weight: 300;
+      }
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    position: sticky;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    top: 0;
+    height: 200px;
+    z-index: 50;
+
+    .header__heading-wrapper {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      margin-bottom: 0;
+
+      .header__title-wrapper {
+        h1 {
+          font-family: "Fraunces";
+          font-size: 64px;
+          font-weight: 300;
+        }
+        h2 {
+          font-size: 32px;
+          font-weight: 100;
+        }
+      }
+    }
+    .header__languages {
+      .header__languages-note {
+        display: flex;
+
+        ${LanguagesIcon} {
+          margin-left: 12px;
+        }
+
+        p {
+          font-family: "Fraunces";
+          font-size: 12px;
+          font-weight: 300;
+        }
+      }
     }
   }
 `
