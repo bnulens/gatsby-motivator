254f7f47ae41b7f64002d54e11e95c24f7b00804
bnulens <brechtnulens@gmail.com>
2021-03-04 15:47:23 +0100

    Removed excess code

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,19 +3,14 @@ import styled from "styled-components"
 
 import NavbarLinks from "./NavbarLinks"
 
-const SocialBar = styled.nav`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-`
-const HeaderNav = ({ children }) => {
-    return (
-        <SocialBar>
-            <NavbarLinks />
-            {children}
-        </SocialBar>
-    )
+const SocialBar = styled.nav``
+
+const HeaderNav = () => {
+  return (
+    <SocialBar>
+      <NavbarLinks />
+    </SocialBar>
+  )
 }
 
 export default HeaderNav
